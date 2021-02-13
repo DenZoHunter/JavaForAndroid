7c002c7aea97e68046d22a474bc74e5cfd5ffcad
Denis <denzohunter@bk.ru>
2021-02-13 14:46:01 +0300

    Done homework #3 (Ver. 3.0) - final

--- src/main/java/ru/geekbrains/lesson3/Lesson3.java
+++ src/main/java/ru/geekbrains/lesson3/Lesson3.java
@@ -4,25 +4,25 @@ import java.util.Random;
 import java.util.Scanner;
 
 public class Lesson3 {
+
+    private static Scanner scanner = new Scanner(System.in);
+
     public static void main(String[] args) {
 
 //Написать программу, которая загадывает случайное число от 0 до 9 и пользователю дается 3 попытки угадать это число.
 //При каждой попытке компьютер должен сообщить, больше ли указанное пользователем число, чем загаданное, или меньше.
 //После победы или проигрыша выводится запрос – «Повторить игру еще раз? 1 – да / 0 – нет»(1 – повторить, 0 – нет).
 
-        System.out.println("Приветству вас! Добро пожалость в игру - Угадай число!");
+        System.out.println("Приветствую вас! Добро пожаловать в игру - Угадай число!");
         System.out.println("Ваша задача угадать число от 0 до 9, на игру даётся 3 попытки: ");
 
         Random random = new Random();
-        Scanner scanner = new Scanner(System.in);
 
         int randomNumber = random.nextInt(10);
-        int userTryCount = 3;
+        final int USER_TRY_COUNT = 3;
         int userTry = 1;
-        boolean inGame = true;
-        boolean userWon = false;
 
-        while (inGame) {
+        while (true) {
             System.out.println("Введите число: ");
             int userAnswer = scanner.nextInt();
 
@@ -30,33 +30,30 @@ public class Lesson3 {
                 System.out.println("Ваше число больше загаданного!");
             } else if (userAnswer < randomNumber) {
                 System.out.println("Ваше число меньше загаданного!");
-            } else if (userAnswer == randomNumber) {
-                System.out.println("Поздравляю, вы угадали!");
-                userWon = true;
             }
-
-            if (userTry == userTryCount) {
-                System.out.println("Ваши попытки закончились!");
-                inGame = playAgainGame();
-                userTry = 0;
-            } else if (userWon) {
-                inGame = playAgainGame();
-              userTry = 0;
+            if ((userAnswer == randomNumber) || (userTry == USER_TRY_COUNT)) {
+                if (userTry == USER_TRY_COUNT)
+                    System.out.println("Вы проиграли!");
+                else
+                    System.out.println("Поздравляю, вы угадали!");
+                if (!playAgainGame())
+                    break;
+                else {
+                    randomNumber = random.nextInt(10);
+                    userTry = 0;
+                }
             }
             userTry++;
         }
     }
 
     private static boolean playAgainGame() {
-        Scanner scannerAnswer = new Scanner(System.in);
-        int answer;
-
+        int userAnswer;
         do {
             System.out.println("Повторить игру еще раз? 1 – да / 0 – нет: ");
-            answer = scannerAnswer.nextInt();
-        } while (answer < 0 || answer > 1);
-
-        return answer == 1;
+            userAnswer = scanner.nextInt();
+        } while (userAnswer < 0 || userAnswer > 1);
+        return userAnswer == 1;
     }
 }
 
