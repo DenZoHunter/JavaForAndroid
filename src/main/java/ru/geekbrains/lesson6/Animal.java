fe8599ab25d100c7bf31d26b9bbfe5fd3c1c0083
Denis <denzohunter@bk.ru>
2021-02-27 01:02:36 +0300

    Done homework #6 full

---
+++ src/main/java/ru/geekbrains/lesson6/Animal.java
@@ -0,0 +1,19 @@
+package ru.geekbrains.lesson6;
+
+public abstract class Animal {
+
+    protected String name;
+    protected String color;
+    protected int age;
+
+    public Animal(String name, String color, int age) {
+        this.name = name;
+        this.color = color;
+        this.age = age;
+
+    }
+
+    public abstract void run(int distance);
+
+    public abstract void swim(int distance);
+}
